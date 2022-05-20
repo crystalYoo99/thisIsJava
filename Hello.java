e1d9f84542ff701b1730b8333e6b27c7d7d82bff
crystalYoo99 <crystal@handong.ac.kr>
2022-05-20 16:25:52 +0900

    0520

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/*
+작성자 : 유수정
+작성일 : 2022.05.18
+*/
+
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, welcome to the java world!");
+	}//end of main
+}//end of class
